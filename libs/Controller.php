fadf323da352d8a4267e75f39d60842a292748b6
Harrison Stahl <uber1337newb@gmail.com>
2012-10-30 22:20:22 -0500

    i a line

--- libs/Controller.php
+++ libs/Controller.php
@@ -4,12 +4,9 @@ namespace libs;
 class Controller {
 
     function __construct() {
-        
-    }
+        }
 }
  spl_autoload_register(function($className){
         $fileLocation = str_replace('\\',PATH_SEP, $className).".php";
-       // echo $fileLocation;
-        require_once($fileLocation);
-        
+        require_once($fileLocation);   
     });
