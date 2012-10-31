771b8ea68f92f99fc8a43fceef3f84941844f482
Harrison Stahl <uber1337newb@gmail.com>
2012-10-30 22:18:01 -0500

    removed unused code

--- libs/Controller.php
+++ libs/Controller.php
@@ -5,34 +5,7 @@ class Controller {
 
     function __construct() {
         
-        $this->view = new View();
     }
-
-   
-    public function loadModel($name) {
-
-        $path = 'models/' . $name . '_model.php';
-
-        if (file_exists($path)) {
-            require 'models/' . $name . '_model.php';
-
-            $modelName = $name . '_Model';
-            $this->model = new $modelName();
-            echo$modelName;
-        }
-    }
-
-    public function loadSeg($name) {
-        $path = 'models/seg/' . $name . '_model.php';
-
-        if (file_exists($path)) {
-            require 'models/seg/' . $name . '_model.php';
-
-            $modelName = $name . '_Model';
-            $this->$name = new $modelName();
-        }
-    }
-
 }
  spl_autoload_register(function($className){
         $fileLocation = str_replace('\\',PATH_SEP, $className).".php";
