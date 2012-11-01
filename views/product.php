807e50764447ff1bc1007128e09adf53942ce6b2
Harrison Stahl <uber1337newb@gmail.com>
2012-10-31 20:37:33 -0500

    created product shell with add funcitonality

--- views/product.php
+++ views/product.php
@@ -1,7 +1,21 @@
 <?php
+namespace controllers;
+use \controllers\seg as cs;
+class Product extends \libs\Controller {
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+    function __construct() {
+        parent::__construct();
+        $this->model = new \models\Product();
+        $this->view = new \views\Product();
+    }
+    
+    function index(){
+        
+    }
+    
+    function add(){
+        require 'views/header.php';
+        
+        require 'views/footer.php';
+    }
+}
