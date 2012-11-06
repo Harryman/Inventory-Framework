e72506358ee4c5ac87e1ce1f77f403b4949011bd
Harrison Stahl <uber1337newb@gmail.com>
2012-11-06 00:15:28 -0600

    done?

---
+++ models/seg/compatible.php
@@ -0,0 +1,23 @@
+<?php
+namespace models\seg;
+class Compatible extends \libs\Model {
+    public $table = "compatible";
+    function __construct() {
+        parent::__construct();
+    }
+    
+    function insert(){
+       $this->db->query("INSERT INTO prod_cat (`p_id`,`compatible_id`) VALUES (\"".$_POST['p_id']."\",\"".$_POST['compatible_id']."\")");
+    }
+    function get($id){
+        $st = $this->db->query("SELECT `compatible_id` FROM compatible WHERE `p_id`= '".$id."'");
+        $ret = $st->fetchAll(\PDO::FETCH_ASSOC);
+        $result = json_encode($ret);
+        header('Content-Type: application/json');
+        echo $result;
+        return $ret;
+    }
+    function del(){
+        $this->db->query("DELETE FROM compatible WHERE `p_id` =  '".$_POST['p_id']."' AND `compatible_id` = '".$_POST['compatible_id']."'");
+    }
+}
